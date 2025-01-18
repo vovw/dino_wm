31211b1fed3cd328976e9f7449a20283539f6cff
gaoyuezhou <zhougy_99@berkeley.edu>
2025-01-17 19:07:07 -0500

    Initial commit
    cleanup train.py and config
    
    cleanup wandb proj name; openloop rollout

---
+++ utils.py
@@ -0,0 +1,119 @@
+import os
+import numpy as np
+import argparse
+import torch
+import random
+from PIL import Image
+from einops import rearrange
+from omegaconf import DictConfig, OmegaConf, open_dict
+from typing import Callable, Dict
+import psutil
+
+def get_ram_usage():
+    process = psutil.Process(os.getpid())
+    return process.memory_info().rss / (1024 * 1024 * 1024)  # Memory usage in MB
+
+def get_available_ram():
+    mem = psutil.virtual_memory()
+    return mem.available / (1024 * 1024 * 1024)  # Available memory in MB
+
+def dict_to_namespace(cfg_dict):
+    args = argparse.Namespace()
+    for key in cfg_dict:
+        setattr(args, key, cfg_dict[key])
+    return args
+
+def move_to_device(dct, device):
+    for key, value in dct.items():
+        if isinstance(value, torch.Tensor):
+            dct[key] = value.to(device)
+    return dct
+
+def slice_trajdict_with_t(data_dict, start_idx=0, end_idx=None, step=1):
+    if end_idx is None:
+        end_idx = max(arr.shape[1] for arr in data_dict.values())
+    return {key: arr[:, start_idx:end_idx:step, ...] for key, arr in data_dict.items()}
+
+def concat_trajdict(dcts):
+    full_dct = {}
+    for k in dcts[0].keys():
+        if isinstance(dcts[0][k], np.ndarray):
+            full_dct[k] = np.concatenate([dct[k] for dct in dcts], axis=1)
+        elif isinstance(dcts[0][k], torch.Tensor):
+            full_dct[k] = torch.cat([dct[k] for dct in dcts], dim=1)
+        else:
+            raise TypeError(f"Unsupported data type: {type(dcts[0][k])}")
+    return full_dct
+
+def aggregate_dct(dcts):
+    full_dct = {}
+    for dct in dcts:
+        for key, value in dct.items():
+            if key not in full_dct:
+                full_dct[key] = []
+            full_dct[key].append(value)
+    for key, value in full_dct.items():
+        if isinstance(value[0], torch.Tensor):
+            full_dct[key] = torch.stack(value)
+        else:
+            full_dct[key] = np.stack(value)
+    return full_dct
+
+def sample_tensors(tensors, n, indices=None):
+    if indices is None:
+        b = tensors[0].shape[0]
+        indices = torch.randperm(b)[:n]
+    indices = torch.tensor(indices)
+    for i, tensor in enumerate(tensors):
+        if tensor is not None:
+            tensors[i] = tensor[indices]
+    return tensors
+
+
+def cfg_to_dict(cfg):
+    cfg_dict = OmegaConf.to_container(cfg)
+    for key in cfg_dict:
+        if isinstance(cfg_dict[key], list):
+            cfg_dict[key] = ",".join(cfg_dict[key])
+    return cfg_dict
+
+def reduce_dict(f: Callable, d: Dict):
+    return {k: reduce_dict(f, v) if isinstance(v, dict) else f(v) for k, v in d.items()}
+
+def seed(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+
+def pil_loader(path):
+    with open(path, "rb") as f:
+        with Image.open(f) as img:
+            return img.convert("RGB")
+
+# inference
+def eval_emb_to_state(model, X, y_tgt):
+    """
+    y_hat = model(X), solve for (y_tgt - y_hat)^2
+    """
+    state_pred_mse = np.mean((model.predict(X) - y_tgt) ** 2).item()
+    return state_pred_mse
+
+def probe_z_to_s(model, X, y_tgt):
+    '''
+    X: (b, t, p, e)
+    y_tgt: (b, t, d2)
+    '''
+    device = X.device
+    X = rearrange(X, "b t p e -> (b t) p e")
+    X = rearrange(X, "bt p e -> bt (p e)")
+    y_tgt = rearrange(y_tgt, 'b t d -> (b t) d')
+
+    X = X.detach().cpu().numpy()
+    y_tgt = y_tgt.detach().cpu().numpy()
+    y_hat = model.predict(X)
+    pred_mse = torch.tensor(np.mean((y_hat - y_tgt) ** 2), device=device)
+    return pred_mse
+
